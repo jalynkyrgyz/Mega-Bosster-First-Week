bacb3f3da7b22db3971b2f30cc368147b1739254
Jalyn Kyrgyz <87895714+jalynkyrgyz@users.noreply.github.com>
2022-08-11 23:06:42 +0600

    docs: finish fifth task

---
+++ script.js
@@ -0,0 +1,8 @@
+let tasks = ['Buy lemonade', 'Make toasts', 'Repair car', 'Play games', 'Pet a cat'];
+    
+tasks.forEach(function(elem) {
+    const list = document.getElementById("todo-list");
+    const lists = list.appendChild(document.createElement("li"));
+    lists.classList.add("list");
+    lists.textContent = elem;
+}) 
